5b9407fef2a7c26e54ec0f32786346cd02fb70ed
th222fa <th222fa@student.lnu.se>
2014-10-26 14:48:03 +0100

    nu med månadens namn på svenska

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use view\HtmlView;
 require_once("./ImportFiles.php");
 
 session_start();
-setlocale(LC_ALL, "sve");
+setlocale(LC_ALL, "sv_SE.utf8");
 
 $htmlView = new HtmlView();
 $navigationController = new NavigationController();
